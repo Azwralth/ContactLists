4fbcd322f0b280d34a7e83d87b3c4780cdc2cdfe
Vladislav <mackva34@gmail.com>
2024-05-05 10:11:33 +0200

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Владислав Соколов on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
