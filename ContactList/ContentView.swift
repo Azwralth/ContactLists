72d75738d82d3bbd5be541e9c6403b2f47d4ebe5
Vladislav <mackva34@gmail.com>
2024-05-06 00:38:17 +0200

    deleted NavigationStack and added searchController

--- ContactList/ContentView.swift
+++ ContactList/ContentView.swift
@@ -8,24 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    let person = Person.getPersons()
+    let persons = Person.getPersons()
+    
+    @State private var searchName = ""
     
     var body: some View {
-        NavigationStack {
-            TabView {
-                PersonListView(persons: person)
-                    .tabItem {
-                        Image(systemName: "person.2.circle")
-                        Text("Contacts")
-                    }
-                PersonSectionView(persons: person)
-                    .tabItem {
-                        Image(systemName: "phone")
-                        Text("Numbers")
-                    }
-            }
-            .navigationTitle("Contact List")
-            .preferredColorScheme(.dark)
+        TabView {
+            PersonListView(persons: searchPersons)
+                .tabItem {
+                    Image(systemName: "person.2.circle")
+                    Text("Contacts")
+                }
+            PersonSectionView(persons: searchPersons)
+                .tabItem {
+                    Image(systemName: "phone")
+                    Text("Numbers")
+                }
+        }
+        .preferredColorScheme(.dark)
+        .searchable(text: $searchName, prompt: "Search")
+    }
+    
+    private var searchPersons: [Person] {
+        if searchName.isEmpty {
+            return persons
+        } else {
+            return persons.filter { $0.fullName.contains(searchName) }
         }
     }
 }
